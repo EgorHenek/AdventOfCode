3d9e5735738d2885af7b2baf7452b53ac18f03ae
Egor Glukhikh <eagluhih@gmail.com>
2025-12-01 15:52:37 +0500

    day 1 part 2

--- src/aoc/day1.py
+++ src/aoc/day1.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 
 
-def main(path: Path) -> int:
+def part1(path: Path) -> int:
     zero_counter = 0
 
     with path.open("r") as file:
@@ -23,7 +23,37 @@ def main(path: Path) -> int:
     return zero_counter
 
 
+def part2(path: Path) -> int:
+    zero_counter = 0
+
+    with path.open("r") as file:
+        lines = file.readlines()
+
+        rotations: list[int] = []
+        for line in lines:
+            rotations.append(int(line[1:]))
+            if line[0] == "L":
+                rotations[-1] *= -1
+
+        current_position = 50
+        for rotation in rotations:
+            start_pos = current_position
+            end_pos = start_pos + rotation
+            crossings = 0
+            if rotation > 0:
+                crossings = end_pos // 100 - start_pos // 100
+            elif rotation < 0:
+                crossings = (start_pos - 1) // 100 - (end_pos - 1) // 100
+            zero_counter += crossings
+            current_position = end_pos % 100
+    return zero_counter
+
+
 if __name__ == "__main__":
     path = Path("./inputs/day1.txt")
-    result = main(path)
-    print(result)
+
+    result_part1 = part1(path)
+    result_part2 = part2(path)
+
+    print(f"Part 1: {result_part1}")
+    print(f"Part 2: {result_part2}")
--- tests/test_day1.py
+++ tests/test_day1.py
@@ -1,11 +1,19 @@
 from pathlib import Path
 
-from aoc.day1 import main
+from aoc.day1 import part1, part2
 
 
-def test_example_input() -> None:
+def test_part1() -> None:
     path = Path("./inputs/day1_example.txt")
 
-    result = main(path)
+    result = part1(path)
 
     assert result == 3
+
+
+def test_part2() -> None:
+    path = Path("./inputs/day1_example.txt")
+
+    result = part2(path)
+
+    assert result == 6
