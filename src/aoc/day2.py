00cd772de526200ce83da718dae9a9adbf1ac06b
Egor Glukhikh <eagluhih@gmail.com>
2025-12-04 16:50:54 +0500

    day 2 part 2

--- src/aoc/day2.py
+++ src/aoc/day2.py
@@ -18,9 +18,29 @@ def part1(path: Path) -> int:
     return result
 
 
+def part2(path: Path) -> int:
+    result = 0
+    with path.open("r") as file:
+        ranges = file.readline().split(",")
+        ranges = map(lambda x: map(int, x.split("-")), ranges)
+        for ids_range in ranges:
+            start = next(ids_range)
+            end = next(ids_range)
+            for i in range(start, end + 1):
+                str_i = str(i)
+                n = len(str_i)
+                for window_size in range(1, (n // 2) + 1):
+                    if str_i == str_i[:window_size] * (n // window_size):
+                        result += i
+                        break
+    return result
+
+
 if __name__ == "__main__":
     path = Path("./inputs/day2.txt")
 
     result_part1 = part1(path)
+    result_part2 = part2(path)
 
-    print(f"Part 1: {result_part1}")
+    print(f"{result_part1=}")
+    print(f"{result_part2=}")
--- tests/test_day2.py
+++ tests/test_day2.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from aoc.day2 import part1
+from aoc.day2 import part1, part2
 
 
 def test_part1() -> None:
@@ -8,3 +8,11 @@ def test_part1() -> None:
     result = part1(path)
 
     assert result == 1227775554
+
+
+def test_part2() -> None:
+    path = Path("./inputs/day2_example.txt")
+
+    result = part2(path)
+
+    assert result == 4174379265
