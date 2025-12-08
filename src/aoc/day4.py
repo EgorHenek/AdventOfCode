1a092ab58880cfbe8faf73e19d30d9e86ace31f7
Egor Glukhikh <eagluhih@gmail.com>
2025-12-08 21:51:20 +0500

    day 4 part 2

--- src/aoc/day4.py
+++ src/aoc/day4.py
@@ -21,12 +21,36 @@ def part1(path: Path) -> int:
     return result
 
 
-def part2(path: Path) -> int: ...
+def part2(path: Path) -> int:
+    result = 0
+    with path.open("r") as file:
+        lines = file.readlines()
+        lines = [line.strip() for line in lines]
+        lines = [[ch == "@" for ch in line] for line in lines]
+        while True:
+            old_result = result
+            for n in range(len(lines)):
+                for m in range(len(lines[n])):
+                    if lines[n][m]:
+                        adjacent_count = sum(
+                            lines[i][j]
+                            for i in range(max(n - 1, 0), min(n + 2, len(lines)))
+                            for j in range(max(m - 1, 0), min(m + 2, len(lines[0])))
+                            if i != n or j != m
+                        )
+                        if adjacent_count < 4:
+                            result += 1
+                            lines[n][m] = False
+            if old_result == result:
+                break
+    return result
 
 
 if __name__ == "__main__":
     path = Path("./inputs/day4.txt")
 
     result_part1 = part1(path)
+    result_part2 = part2(path)
 
     print(f"{result_part1=}")
+    print(f"{result_part2=}")
--- tests/test_day4.py
+++ tests/test_day4.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from aoc.day4 import part1
+from aoc.day4 import part1, part2
 
 
 def test_part1() -> None:
@@ -8,3 +8,11 @@ def test_part1() -> None:
     result = part1(path)
 
     assert result == 13
+
+
+def test_part2() -> None:
+    path = Path("./inputs/day4_example.txt")
+
+    result = part2(path)
+
+    assert result == 43
