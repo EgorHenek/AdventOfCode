633bd6e49a0555064d2c8d54f493a6a71d2ddda9
Egor Glukhikh <eagluhih@gmail.com>
2025-12-09 19:03:14 +0500

    day 5 part 2

--- src/aoc/day5.py
+++ src/aoc/day5.py
@@ -1,4 +1,5 @@
 # https://adventofcode.com/2025/day/5
+from heapq import heapify, heappop
 from pathlib import Path
 
 
@@ -26,9 +27,34 @@ def part1(path: Path) -> int:
     return result
 
 
+def part2(path: Path) -> int:
+    result = 0
+    with path.open("r") as file:
+        lines = file.readlines()
+        lines = map(lambda line: line.strip(), lines)
+        lines = filter(lambda line: line.count("-"), lines)
+        lines = [(int(line.split("-")[0]), int(line.split("-")[1])) for line in lines]
+        heapify(lines)
+
+        fresh_ranges = [heappop(lines)]
+
+        while lines:
+            start, end = heappop(lines)
+            if fresh_ranges[-1][0] <= start <= fresh_ranges[-1][1]:
+                fresh_ranges[-1] = fresh_ranges[-1][0], max(end, fresh_ranges[-1][1])
+            else:
+                fresh_ranges.append((start, end))
+
+    for start, end in fresh_ranges:
+        result += end - start + 1
+    return result
+
+
 if __name__ == "__main__":
     path = Path("./inputs/day5.txt")
 
     result_part1 = part1(path)
+    result_part2 = part2(path)
 
     print(f"{result_part1=}")
+    print(f"{result_part2=}")
