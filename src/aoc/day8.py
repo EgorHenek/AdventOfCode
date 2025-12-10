393b80faa8ac0572864aedcb868f1db024f26ca7
Egor Glukhikh <eagluhih@gmail.com>
2025-12-10 20:09:52 +0500

    day 8 part 2

--- src/aoc/day8.py
+++ src/aoc/day8.py
@@ -71,9 +71,63 @@ def part1(path: Path, conntections_limit: int) -> int:
         )
 
 
+def part2(path: Path) -> int:
+    with path.open() as file:
+        points: list[Point] = []
+        while line := file.readline():
+            x, y, z = line.split(",")
+            points.append(Point(int(x), int(y), int(z)))
+        parent = [i for i in range(len(points))]
+        component_size = [1] * len(points)
+
+        num_components = len(points)
+
+        def find_root(i: int) -> int:
+            if parent[i] == i:
+                return i
+            parent[i] = find_root(parent[i])
+            return parent[i]
+
+        def union(i: int, j: int) -> bool:
+            root_i = find_root(i)
+            root_j = find_root(j)
+            if root_i == root_j:
+                return False
+            if component_size[root_i] < component_size[root_j]:
+                parent[root_i] = root_j
+                component_size[root_j] += component_size[root_i]
+            else:
+                parent[root_j] = root_i
+                component_size[root_i] += component_size[root_j]
+            return True
+
+        edges: list[tuple[int, int, int]] = []
+        for i in range(len(points) - 1):
+            for j in range(i + 1, len(points)):
+                point1 = points[i]
+                point2 = points[j]
+                euclidean_distance = (
+                    (point2.x - point1.x) ** 2
+                    + (point2.y - point1.y) ** 2
+                    + (point2.z - point1.z) ** 2
+                )
+                edges.append((euclidean_distance, i, j))
+        edges.sort()
+
+        for edge in edges:
+            if union(edge[1], edge[2]):
+                num_components -= 1
+
+                if num_components == 1:
+                    return points[edge[1]].x * points[edge[2]].x
+        return 0
+
+
 if __name__ == "__main__":
     path = Path("./inputs/day8.txt")
 
     result_part1 = part1(path, 1000)
+    result_part2 = part2(path)
 
     print(f"{result_part1=}")
+    print(f"{result_part2=}")
--- tests/test_day8.py
+++ tests/test_day8.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from aoc.day8 import part1
+from aoc.day8 import part1, part2
 
 
 def test_part1() -> None:
@@ -8,3 +8,11 @@ def test_part1() -> None:
     result = part1(path, 10)
 
     assert result == 40
+
+
+def test_part2() -> None:
+    path = Path("./inputs/day8_example.txt")
+
+    result = part2(path)
+
+    assert result == 25272
